cd687b8adb97f2afa082c35cfd4c756591d104e6
ryandaryl <ryandarylmills@gmail.com>
2026-05-22 00:31:27 +1000

    Write exec output directly to file

--- server.py
+++ server.py
@@ -1,22 +1,30 @@
 from ast import iter_child_nodes, parse, Expr, Module, Expression
-import io
 from contextlib import redirect_stdout
+import hashlib
+import io
+import os
+import tempfile
 import traceback
 
 def execute_and_collect(code_to_run, con):
-    out_buffer = io.StringIO()
+    f = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
     error_string = ""
     args = {"filename": "myfile.py", "mode": "eval"}
     try:
-        with redirect_stdout(out_buffer):
+        with redirect_stdout(f):
             nodes, last = list(iter_child_nodes(parse(code_to_run))), ""
             if isinstance(nodes[-1], Expr):
                 nodes, last = nodes[:-1], nodes[-1]
             exec(compile(Module(body=nodes), **{**args, "mode": "exec"}), con)
             print(eval(compile(Expression(body=last.value), **args), con))
     except Exception as e:
-        error_string = traceback.format_exc()
-    return out_buffer.getvalue(), error_string
+        f.write(traceback.format_exc())
+    finally:
+        f.seek(0)
+        print(f.read())
+        with open(f.name, "rb") as f2:
+            os.rename(f.name, hashlib.file_digest(f2, "md5").hexdigest())
+        f.close()
 
 
 code_to_run1 = """
@@ -29,6 +37,5 @@ code_to_run2 = "a"
 
 context_global = {}
 for code_to_run in [code_to_run1, code_to_run2]:
-    out_string, error_string = execute_and_collect(code_to_run, context_global)
-    print(f"Captured STDOUT: {out_string}")
-    print(f"Captured STDERR: {error_string}")
+    execute_and_collect(code_to_run, context_global)
+
